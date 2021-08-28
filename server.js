c4492e6d5b6f9f72ebf93b11e904381e78ef4c7f
jeffawheeler23 <jeffawheeler23@gmail.com>
2021-08-28 17:11:47 -0400

    connected to plaid API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const plaid = require("./routes/api/plaid");
 
 const app = express();
 // Bodyparser middleware
@@ -25,6 +26,7 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+app.use("/api/plaid", plaid);
 
 const port = process.env.PORT || 8000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
